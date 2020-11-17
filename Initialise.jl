116d743e7f1db0612799e25141f508829cd17f07
chris-revell <christopher.revell@crick.ac.uk>
2020-11-17 20:36:58 +0000

    Create Initialise.jl

---
+++ Initialise.jl
@@ -0,0 +1,40 @@
+#
+#  Initialise.jl
+#  collagen-diffusive-rods
+#
+#  Created by Christopher Revell on 17/11/2020.
+#
+#
+
+module Initialise
+
+using LinearAlgebra
+using StaticArrays
+using Random
+include("./OutputData.jl")
+using .OutputData
+
+@inline @views function initialise!(N,r,Ω,boxSize,threadRNG,nthreads,outputToggle)
+    Random.seed!(3)
+    # Create random number generators for each thread
+    for i in 1:nthreads
+        threadRNG[i] = Random.MersenneTwister(3)
+    end
+
+    # Initialise random rod positions
+    for i=1:N
+        r[i] = SVector{3}((rand(threadRNG[1],Float64,3).-0.5).*boxSize)
+    end
+    # Initialise random rod orientations
+    for i=1:N
+        Ω[i] = SVector{3}(rand(threadRNG[1],Float64,3).-0.5)
+        # Normalise magnitude
+        Ω[i] = Ω[i]./sqrt(Ω[i]⋅Ω[i])
+    end
+
+    return nothing
+end
+
+export initialise!
+
+end
